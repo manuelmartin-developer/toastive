fc7d65df67ebec0b7f359ef590ee0cdfb5b9e676
Manuel Martin <mmartin@mobbeel.com>
2025-01-21 22:08:43 +0100

    chore: update ESLint configuration to ignore additional directories

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import tseslint from 'typescript-eslint'
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{ files: ['**/*.{js,mjs,cjs,ts}'] },
-	{ ignores: ['node_modules', 'dist', 'build', 'coverage', 'public'] },
+	{ ignores: ['node_modules', 'dist', 'build', 'coverage', 'public', '__docs__', 'storybook-static'] },
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended
